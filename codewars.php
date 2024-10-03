75db2564f96ba8c94aa6931a7291c2a07df14beb
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2024-10-04 00:06:42 +0100

    add: codewar projects

--- codewars.php
+++ codewars.php
@@ -1,1 +1,14 @@
 <?php
+function countsheep($num)
+{
+    $murmur = '';
+    for ($i = 1; $i <= $num; $i++) {
+        if ($num === 0) $murmur .= '';
+        else {
+            $murmur .= "$i sheep...";
+        }
+    }
+    return $murmur;
+};
+
+echo countsheep(4);
